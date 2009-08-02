f83d7a3a71fe4310627716b92a2a3193058f0b48
Shandem <unknown@kiln.example.com>
2009-08-02 11:51:00 +0000

    Changed project names, file paths, etc...

---
+++ umbracoexamine/trunk/UmbracoExamine.Core/IndexerData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace UmbracoExamine
+{
+    /// <summary>
+    /// a data structure for storing indexing instructions
+    /// </summary>
+    public class IndexerData
+    {
+
+        public IndexerData(string[] umbracoFields, string[] userFields, string indexPath, string[] includeNodeTypes, string[] excludeNodeTypes, int? parentNodeId, int maxResults)
+        {
+            IndexPath = indexPath;
+            UserFields = userFields;
+            UmbracoFields = umbracoFields;
+            IncludeNodeTypes = includeNodeTypes;
+            ExcludeNodeTypes = excludeNodeTypes;
+            ParentNodeId = parentNodeId;
+            IndexDirectory = new DirectoryInfo(IndexPath);
+            MaxResults = maxResults;
+        }
+
+        public string IndexPath { get; private set; }
+        public string[] UmbracoFields { get; private set; }
+        public string[] UserFields { get; private set; }
+        public string[] IncludeNodeTypes { get; private set; }
+        public string[] ExcludeNodeTypes { get; private set; }
+        public int? ParentNodeId { get; private set; }
+        public DirectoryInfo IndexDirectory { get; private set; }
+        public int MaxResults { get; private set; }
+    }
+}
---
+++ umbracoexamine/trunk/UmbracoExamine.Core/IndexingNodeEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UmbracoExamine
+{
+    public class IndexingNodeEventArgs : EventArgs 
+    {
+
+        public IndexingNodeEventArgs(int nodeId)
+        {
+            this.NodeId = nodeId;
+        }
+
+        public int NodeId { get; private set; }
+
+    }
+}
