ebaebe962eea65a9d0d22fd3eba13666c51195bd
slace <unknown@kiln.example.com>
2010-03-16 22:21:43 +0000

    Cleaning up the assembly manifest info

---
+++ Solution Items/SolutionInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+[assembly: AssemblyCompany("TheFARM")]
+[assembly: AssemblyCopyright("Copyright © TheFARM 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("en-AU")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- UmbracoExamine.Providers/Properties/AssemblyInfo.cs
+++ UmbracoExamine.Providers/Properties/AssemblyInfo.cs
@@ -1,18 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UmbracoExamine.Providers")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Umbraco Examine Lucene.NET providers")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("UmbracoExamine.Providers")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("31c5b048-cfa8-49b4-8983-bdba0f99eef5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
