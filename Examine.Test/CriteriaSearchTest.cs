2673340342c526a07bdfd498000e5b314f46f08b
ismailm <unknown@kiln.example.com>
2010-08-20 14:09:53 +0000

    Added new class to testsuite to try criteria tests. the orderby test fails no results returned looks like cws_textpage when pushed through standard analyser writes query out as nodeTypeAlias:"cws textpage" and thus you get no results keyword analyser writes it out correctly was the test index created using keyword analyser?  I had added test due to issue http://our.umbraco.org/forum/developers/extending-umbraco/11863-Examine-sort-of-results thinking it may be becuase i am not using fluent criteria

---
+++ Examine.Test/CriteriaSearchTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Examine.SearchCriteria;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UmbracoExamine;
+
+namespace Examine.Test
+{
+    [TestClass]
+    public class CriteriaSearchTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        private static IndexInitializer m_Init;
+        private static ISearcher m_Searcher;
+        private static IIndexer m_Indexer;
+
+        #region Initialize and Cleanup
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            m_Init = new IndexInitializer();
+
+            m_Searcher = ExamineManager.Instance.SearchProviderCollection["CWSSearcher"];
+            m_Indexer = ExamineManager.Instance.IndexProviderCollection["CWSIndexer"];
+        }
+
+        [TestMethod]
+        public void CriteriaSearch_Cws_TextPage_OrderedByNodeName()
+        {
+             var criteria = m_Searcher.CreateSearchCriteria(IndexTypes.Content);
+            IBooleanOperation query = criteria.NodeTypeAlias("cws_textpage");
+             query = query.And().OrderBy("nodeName");
+             var sCriteria = query.Compile();
+             Console.WriteLine(sCriteria.ToString());
+             var results = m_Searcher.Search(sCriteria);
+
+             criteria = m_Searcher.CreateSearchCriteria(IndexTypes.Content);
+             IBooleanOperation query2 = criteria.NodeTypeAlias("cws_textpage");
+             query2 = query2.And().OrderByDescending("nodeName");
+             var sCriteria2 = query2.Compile();
+             Console.WriteLine(sCriteria2.ToString());
+             var results2 = m_Searcher.Search(sCriteria2);
+
+             Assert.AreNotEqual(results.First().Id, results2.First().Id);
+             
+        }
+
+        //[ClassCleanup()]
+        //public static void Cleanup()
+        //{
+
+        //}
+
+        #endregion
+    }
+}
