4d19d00b48119c24a74161bece9a3ef841eaba22
Shandem <unknown@kiln.example.com>
2011-09-26 17:48:25 -1000

    Fixed invalid xml/hex chars issue

--- Examine/DictionaryExtensions.cs
+++ Examine/DictionaryExtensions.cs
@@ -8,12 +8,59 @@ using System.Xml.Linq;
 
 namespace Examine
 {
+    ///<summary>
+    /// Extensions for Dictionary objects
+    ///</summary>
     public static class DictionaryExtensions
     {
         ///<summary>
         /// The encoding to use when saving the file
         ///</summary>
         internal static Encoding DefaultFileEncoding = Encoding.UTF8;
+
+        /// <summary>
+        /// Remove illegal XML characters from a string.
+        /// </summary>
+        private static string SanitizeXmlString(string xml)
+        {
+            if (xml == null)
+            {
+                throw new ArgumentNullException("xml");
+            }
+
+            var buffer = new StringBuilder(xml.Length);
+
+            foreach (var c in xml.Where(c => IsLegalXmlChar(c)))
+            {
+                buffer.Append(c);
+            }
+
+            return buffer.ToString();
+        }
+
+        /// <summary>
+        /// Whether a given character is allowed by XML 1.0.
+        /// </summary>
+        private static bool IsLegalXmlChar(int character)
+        {
+            return
+            (
+                 character == 0x9 /* == '\t' == 9   */          ||
+                 character == 0xA /* == '\n' == 10  */          ||
+                 character == 0xD /* == '\r' == 13  */          ||
+                (character >= 0x20 && character <= 0xD7FF) ||
+                (character >= 0xE000 && character <= 0xFFFD) ||
+                (character >= 0x10000 && character <= 0x10FFFF)
+            );
+        }
+
+        private static void CleanValues<TKey>(this Dictionary<TKey, string> items)
+        {
+            foreach(var i in items.Keys.ToArray())
+            {
+                items[i] = SanitizeXmlString(items[i]);
+            }
+        }
 
         public static void SaveToDisk(this IEnumerable<Dictionary<string, string>> items, FileInfo fi)
         {
@@ -22,6 +69,8 @@ namespace Examine
             xml.Add(root);
             foreach (var i in items)
             {
+                CleanValues(i);
+
                 var docRoot = new XElement("document");
                 root.Add(docRoot);
                 foreach(var d in i)
@@ -34,6 +83,8 @@ namespace Examine
 
         public static void SaveToDisk<TKey>(this Dictionary<TKey, string> d, FileInfo fi)
         {
+            CleanValues(d);
+
             var xml = new XDocument();
             var root = new XElement("document");
             xml.Add(root);
