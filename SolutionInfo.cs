583152e0954f38f801d44168546d74d8f551917a
Shannon Deminick <sdeminick@gmail.com>
2013-01-05 02:46:31 +0300

    Changed version to 1.4.3

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -7,5 +7,5 @@ using System.Security;
 [assembly: AssemblyCulture("")]
 
 
-[assembly: AssemblyVersion("0.1.42.2941")]
-[assembly: AssemblyFileVersion("0.1.42.2941")]
+[assembly: AssemblyVersion("0.1.43.2941")]
+[assembly: AssemblyFileVersion("0.1.43.2941")]
