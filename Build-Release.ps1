a65aa8e45003606ab20efcf9896357c2b197bd0e
Shannon Deminick <sdeminick@gmail.com>
2013-02-14 06:06:35 +0600

    Updated build script/version added nuspec

--- Build-Release.ps1
+++ Build-Release.ps1
@@ -9,18 +9,7 @@ $PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path).FullName
 
 $SolutionRoot = Split-Path -Path $PSScriptFilePath -Parent
 
-$Is64BitSystem = (Get-WmiObject -Class Win32_OperatingSystem).OsArchitecture -eq "64-bit"
-$Is64BitProcess = [IntPtr]::Size -eq 8
-
-$RegistryArchitecturePath = ""
-if ($Is64BitProcess) { $RegistryArchitecturePath = "\Wow6432Node" }
-
-$FrameworkArchitecturePath = ""
-if ($Is64BitSystem) { $FrameworkArchitecturePath = "64" }
-
-$ClrVersion = (Get-ItemProperty -path "HKLM:\SOFTWARE$RegistryArchitecturePath\Microsoft\VisualStudio\10.0")."CLR Version"
-
-$MSBuild = "$Env:SYSTEMROOT\Microsoft.NET\Framework$FrameworkArchitecturePath\$ClrVersion\MSBuild.exe"
+$MSBuild = "$Env:SYSTEMROOT\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
 
 # Make sure we don't have a release folder for this version already
 $BuildFolder = Join-Path -Path $SolutionRoot -ChildPath "build";
@@ -51,44 +40,20 @@ if (-not $?)
 }
 
 $CoreExamineFolder = Join-Path -Path $ReleaseFolder -ChildPath "Examine";
-$UmbExamineFolder = Join-Path -Path $ReleaseFolder -ChildPath "UmbracoExamine";
-$UmbExaminePDFFolder = Join-Path -Path $ReleaseFolder -ChildPath "UmbracoExaminePDF";
 $WebExamineFolder = Join-Path -Path $ReleaseFolder -ChildPath "ExamineWebDemo";
 $ExamineAzureFolder = Join-Path -Path $ReleaseFolder -ChildPath "Examine.Azure";
-$UmbracoExamineAzureFolder = Join-Path -Path $ReleaseFolder -ChildPath "UmbracoExamine.Azure";
-$UmbracoExaminePDFAzureFolder  = Join-Path -Path $ReleaseFolder -ChildPath "UmbracoExamine.PDF.Azure";
-
 
 New-Item $CoreExamineFolder -Type directory
-New-Item $UmbExamineFolder -Type directory
-New-Item $UmbExaminePDFFolder -Type directory
 New-Item $WebExamineFolder -Type directory
 New-Item $ExamineAzureFolder -Type directory
-New-Item $UmbracoExamineAzureFolder -Type directory
-New-Item $UmbracoExaminePDFAzureFolder -Type directory
 
 $include = @('*Examine*.dll','*Lucene*.dll','ICSharpCode.SharpZipLib.dll')
 $CoreExamineBinFolder = Join-Path -Path $SolutionRoot -ChildPath "Projects\Examine\bin\Release";
 Copy-Item "$CoreExamineBinFolder\*.dll" -Destination $CoreExamineFolder -Include $include
 
-$UmbExamineBinFolder = Join-Path -Path $SolutionRoot -ChildPath "Projects\UmbracoExamine\bin\Release";
-Copy-Item "$UmbExamineBinFolder\*.dll" -Destination $UmbExamineFolder -Include $include
-
-$include = @('UmbracoExamine.PDF.dll','itextsharp.dll')
-$UmbExaminePDFBinFolder = Join-Path -Path $SolutionRoot -ChildPath "Projects\UmbracoExamine.PDF\bin\Release";
-Copy-Item "$UmbExaminePDFBinFolder\*.dll" -Destination $UmbExaminePDFFolder -Include $include
-
 $include = @('*Examine*.dll','*Lucene*.dll', '*Azure*.dll','ICSharpCode.SharpZipLib.dll')
 $ExamineAzureBinFolder = Join-Path -Path $SolutionRoot -ChildPath "Projects\Examine.Azure\bin\Release";
 Copy-Item "$ExamineAzureBinFolder\*.dll" -Destination $ExamineAzureFolder -Include $include
-
-$UmbracoExamineAzureBinFolder = Join-Path -Path $SolutionRoot -ChildPath "Projects\UmbracoExamine.Azure\bin\Release";
-Copy-Item "$UmbracoExamineAzureBinFolder\*.dll" -Destination $UmbracoExamineAzureFolder -Include $include
-
-$include = @('UmbracoExamine.PDF.Azure.dll','UmbracoExamine.PDF.dll','itextsharp.dll')
-$UmbExaminePDFAzureBinFolder = Join-Path -Path $SolutionRoot -ChildPath "Projects\UmbracoExamine.PDF.Azure\bin\Release";
-Copy-Item "$UmbExaminePDFAzureBinFolder\*.dll" -Destination $UmbracoExaminePDFAzureFolder -Include $include
-
 
 $ExamineWebDemoFolder = Join-Path -Path $SolutionRoot -ChildPath "Projects\Examine.Web.Demo";
 Copy-Item "$ExamineWebDemoFolder\*" -Destination $WebExamineFolder -Recurse
