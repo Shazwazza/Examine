5247966bc65fec6d4a3a29332120360a68778b6f
Shannon <sdeminick@gmail.com>
2015-03-09 14:54:22 +1100

    bumps version

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -7,5 +7,5 @@ using System.Security;
 [assembly: AssemblyCulture("")]
 
 
-[assembly: AssemblyVersion("0.1.60.2941")]
-[assembly: AssemblyFileVersion("0.1.60.2941")]
+[assembly: AssemblyVersion("0.1.61.2941")]
+[assembly: AssemblyFileVersion("0.1.61.2941")]
