46736f61d851eb5e195c8ebd0b2ece9cd932c3a3
Shannon <sdeminick@gmail.com>
2015-08-10 17:42:56 +0200

    bumps version

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -7,6 +7,6 @@ using System.Security;
 [assembly: AssemblyCulture("")]
 
 
-[assembly: AssemblyVersion("0.1.66.0")]
-[assembly: AssemblyFileVersion("0.1.66.0")]
-[assembly: AssemblyInformationalVersion("0.1.66.0")]
+[assembly: AssemblyVersion("0.1.67.0")]
+[assembly: AssemblyFileVersion("0.1.67.0")]
+[assembly: AssemblyInformationalVersion("0.1.67.0")]
