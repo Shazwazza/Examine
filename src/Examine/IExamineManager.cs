c37b254a531ada7c0f06d8f80e46a008e03020e4
Shannon <sdeminick@gmail.com>
2018-03-29 16:01:38 +1100

    missing file

---
+++ src/Examine/IExamineManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Examine
+{
+    public interface IExamineManager
+    {
+        IReadOnlyDictionary<string, IIndexer> IndexProviders { get; }
+
+        void AddIndexer(string name, IIndexer indexer);
+        void AddSearcher(string name, ISearcher searcher);
+        void DeleteFromIndexes(string nodeId);
+        void DeleteFromIndexes(string nodeId, IEnumerable<IIndexer> providers);
+        void Dispose();
+        IIndexer GetIndexer(string indexerName);
+        ISearcher GetSearcher(string indexerName);
+        void IndexAll(string indexCategory);
+        void IndexItems(ValueSet[] nodes);
+        void IndexItems(ValueSet[] nodes, IEnumerable<IIndexer> providers);
+        void RebuildIndexes();
+    }
+}
