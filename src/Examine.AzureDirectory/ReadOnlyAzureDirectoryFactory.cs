5ebc18ee82e5e8f63b616fe9d1a47444b141ebec
Arkadiusz Biel <abi@novicell.co.uk>
2020-11-30 10:13:57 +0000

    correct definition

--- src/Examine.AzureDirectory/ReadOnlyAzureDirectoryFactory.cs
+++ src/Examine.AzureDirectory/ReadOnlyAzureDirectoryFactory.cs
@@ -13,7 +13,7 @@ namespace Examine.AzureDirectory
     /// <summary>
     /// The <see cref="IDirectoryFactory"/> for storing master index data in Blob storage for user on the server that only reads from the index
     /// </summary>
-    public class ReadOnlyAzureDirectoryFactory : AzureDirectoryFactory, IDirectoryFactory2
+    public class ReadOnlyAzureDirectoryFactory : AzureDirectoryFactory, IDirectoryFactory
     {
         private readonly bool _isReadOnly = true;
         public override Lucene.Net.Store.Directory CreateDirectory(DirectoryInfo luceneIndexFolder)
