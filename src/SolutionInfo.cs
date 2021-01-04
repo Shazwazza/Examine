d6fc6c6e67c790ed048bc3489f61a966d3b8bd08
Shannon <sdeminick@gmail.com>
2021-01-04 15:03:57 +1100

    bumps version

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 using System.Security;
 
 [assembly: AssemblyCompany("Shannon Deminick")]
-[assembly: AssemblyCopyright("Copyright © Shannon Deminick 2020")]
+[assembly: AssemblyCopyright("Copyright © Shannon Deminick 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
