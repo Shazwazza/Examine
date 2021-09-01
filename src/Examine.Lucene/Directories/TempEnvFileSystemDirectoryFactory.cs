710cb6082fc774f6f4213f5be16737b886d53501
Shannon <sdeminick@gmail.com>
2021-09-01 15:27:15 -0600

    Fixes TempEnvFileSystemDirectoryFactory GetTempPath doesn't play nice cross platform #243

--- src/Examine.Lucene/Directories/TempEnvFileSystemDirectoryFactory.cs
+++ src/Examine.Lucene/Directories/TempEnvFileSystemDirectoryFactory.cs
@@ -24,7 +24,7 @@ namespace Examine.Lucene.Directories
             var appDomainHash = applicationIdentifier.GetApplicationUniqueIdentifier().GenerateHash();
             
             var cachePath = Path.Combine(
-                Environment.ExpandEnvironmentVariables("%temp%"),
+                Path.GetTempPath(),
                 "ExamineIndexes",
                 //include the appdomain hash is just a safety check, for example if a website is moved from worker A to worker B and then back
                 // to worker A again, in theory the %temp%  folder should already be empty but we really want to make sure that its not
