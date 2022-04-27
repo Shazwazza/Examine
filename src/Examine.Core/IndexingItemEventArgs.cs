78ee4028bbeab07d376c869d7bbb80fd7ab13b31
Shannon <sdeminick@gmail.com>
2022-04-27 13:42:08 +1000

    Allows setting ValueSet on IndexingItemEventArgs

--- src/Examine.Core/IndexingItemEventArgs.cs
+++ src/Examine.Core/IndexingItemEventArgs.cs
@@ -1,11 +1,19 @@
+using System;
 using System.ComponentModel;
 
 namespace Examine
 {
     public class IndexingItemEventArgs : CancelEventArgs
     {
+        private ValueSet _valueSet;
+
         public IIndex Index { get; }
-        public ValueSet ValueSet { get; }
+
+        public ValueSet ValueSet
+        {
+            get => _valueSet;
+            set => _valueSet = value ?? throw new ArgumentNullException("value");
+        }
 
         public IndexingItemEventArgs(IIndex index, ValueSet valueSet)
         {
