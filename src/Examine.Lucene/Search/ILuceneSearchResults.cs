df4da35ced9f3e5288cd3525d818459a72360875
Chad Currie <chad.d.currie@gmail.com>
2022-12-12 21:52:14 +1300

    Doc

--- src/Examine.Lucene/Search/ILuceneSearchResults.cs
+++ src/Examine.Lucene/Search/ILuceneSearchResults.cs
@@ -1,7 +1,13 @@
 namespace Examine.Lucene.Search
 {
+    /// <summary>
+    /// Lucene.NET Search Results
+    /// </summary>
     public interface ILuceneSearchResults : ISearchResults
     {
+        /// <summary>
+        /// Options for Searching After. Used for efficent deep paging.
+        /// </summary>
         SearchAfterOptions SearchAfter { get; }
     }
 }
