21e762ee3333fae8aecef7d8228a98288dae68b1
Chad Currie <chad.d.currie@gmail.com>
2023-12-07 21:08:23 +1300

    Update comment

--- src/Examine.Lucene/Search/ExamineLuceneDefaultSimilarityType.cs
+++ src/Examine.Lucene/Search/ExamineLuceneDefaultSimilarityType.cs
@@ -6,7 +6,7 @@ namespace Examine.Lucene.Search
     /// <summary>
     /// Default Similarity for Examine Lucene.
     /// </summary>
-    /// <remarks>In Examine V3, this is <see cref="DefaultSimilarity"/>. In Examine V4 this will change to <see cref="BM25Similarity"/></remarks>
+    /// <remarks>In Examine V3 and V4, this is <see cref="DefaultSimilarity"/>. In Examine V5 this will change to <see cref="BM25Similarity"/></remarks>
     public class ExamineLuceneDefaultSimilarityType : LuceneSimilarityTypeBase
     {
         private readonly Lazy<DefaultSimilarity> _similarityLazy = new Lazy<DefaultSimilarity>(() => new DefaultSimilarity());
