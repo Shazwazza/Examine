5055af1a9120e76903a2f6f6673138e58e568432
Chad Currie <chad.d.currie@gmail.com>
2023-12-07 23:12:05 +1300

    Fix dequeue

--- src/Examine.Lucene/Search/FilterChainOpBase.cs
+++ src/Examine.Lucene/Search/FilterChainOpBase.cs
@@ -6,31 +6,46 @@ using Lucene.Net.Search;
 
 namespace Examine.Lucene.Search
 {
+    /// <summary>
+    /// Filter Chain Operation
+    /// </summary>
     public abstract class FilterChainOpBase : IFilterChainStart, IFilterChain
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public FilterChainOpBase()
         {
             ChainOps = new Queue<FilterChain> ();
         }
+
+        /// <summary>
+        /// Chained Filter Operations
+        /// </summary>
         public Queue<FilterChain> ChainOps { get; set; }
 
+        /// <summary>
+        /// Build Chained Filter
+        /// </summary>
+        /// <returns></returns>
         public ChainedFilter Build()
         {
-            Filter[] filters = new Filter[ChainOps.Count];
-            int[] logicArray = new int[ChainOps.Count];
-
-            for (int i = 0; i < ChainOps.Count; i++)
+            var count = ChainOps.Count;
+            var filters = new Filter[count];
+            int[] logicArray = new int[count];
+            for (int i = 0; i < count; i++)
             {
                 var fc = ChainOps.Dequeue();
                 filters[i] = fc.Filter;
                 logicArray[i] = fc.Operation;
             }
 
-            ChainedFilter chainedFilter = new ChainedFilter(filters, logicArray);
+            var chainedFilter = new ChainedFilter(filters, logicArray);
             return chainedFilter;
         }
 
         public abstract IFilterChain Chain(ChainOperation operation, Func<INestedFilter, INestedBooleanFilterOperation> nextFilter);
+
         public abstract IFilterChain Chain(Func<INestedFilter, INestedBooleanFilterOperation> nextFilter);
     }
 }
