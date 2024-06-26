a367eabaa1d60490508e760fb1dfebe2f82921ce
Shannon <sdeminick@gmail.com>
2024-06-26 13:58:25 -0600

    Fixing SyncedFileSystemDirectoryFactory when the main index is corrupted.

--- src/Examine.Lucene/Directories/SyncedFileSystemDirectoryFactory.cs
+++ src/Examine.Lucene/Directories/SyncedFileSystemDirectoryFactory.cs
@@ -24,6 +24,7 @@ namespace Examine.Lucene.Directories
     {
         private readonly DirectoryInfo _localDir;
         private readonly ILoggerFactory _loggerFactory;
+        private readonly ILogger<SyncedFileSystemDirectoryFactory> _logger;
         private ExamineReplicator _replicator;
 
         public SyncedFileSystemDirectoryFactory(
@@ -35,6 +36,7 @@ namespace Examine.Lucene.Directories
         {
             _localDir = localDir;
             _loggerFactory = loggerFactory;
+            _logger = _loggerFactory.CreateLogger<SyncedFileSystemDirectoryFactory>();
         }
 
         protected override Directory CreateDirectory(LuceneIndex luceneIndex, bool forceUnlock)
@@ -42,24 +44,55 @@ namespace Examine.Lucene.Directories
             var path = Path.Combine(_localDir.FullName, luceneIndex.Name);
             var localLuceneIndexFolder = new DirectoryInfo(path);
 
-            Directory mainDir = base.CreateDirectory(luceneIndex, forceUnlock);
+            var mainDir = base.CreateDirectory(luceneIndex, forceUnlock);
+
+            var checker = new CheckIndex(mainDir);
+            // TODO: We can redirect the logging output
+            // checker.InfoStream = 
+            var status = checker.DoCheckIndex();
+
+            if (!status.Clean)
+            {
+                _logger.LogInformation("Checked main director index and it is not clean, attempting to fix {IndexName}...", luceneIndex.Name);
+
+                try
+                {
+                    checker.FixIndex(status);
+                    _logger.LogInformation("Index {IndexName} fixed.", luceneIndex.Name);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "{IndexName} index could not be fixed, it will be deleted.", luceneIndex.Name);
+                }
+            }
+            else
+            {
+                _logger.LogInformation("Checked main director index {IndexName} and it is clean.", luceneIndex.Name);
+            }
 
             // used by the replicator, will be a short lived directory for each synced revision and deleted when finished.
             var tempDir = new DirectoryInfo(Path.Combine(_localDir.FullName, "Rep", Guid.NewGuid().ToString("N")));
 
             if (DirectoryReader.IndexExists(mainDir))
             {
+                IndexWriter indexWriter;
+
                 // when the lucene directory is going to be created, we'll sync from main storage to local
                 // storage before any index/writer is opened.
-                using (var tempMainIndexWriter = new IndexWriter(
-                    mainDir,
-                    new IndexWriterConfig(
-                        LuceneInfo.CurrentVersion,
-                        new StandardAnalyzer(LuceneInfo.CurrentVersion))
-                    {
-                        OpenMode = OpenMode.APPEND,
-                        IndexDeletionPolicy = new SnapshotDeletionPolicy(new KeepOnlyLastCommitDeletionPolicy())
-                    }))
+
+                try
+                {
+                    indexWriter = GetIndexWriter(mainDir, OpenMode.APPEND);
+                }
+                catch (Exception ex)
+                {
+                    // Index is corrupted, typically this will be FileNotFoundException
+                    _logger.LogError(ex, "{IndexName} index is corrupt, a new one will be created", luceneIndex.Name);
+
+                    indexWriter = GetIndexWriter(mainDir, OpenMode.CREATE);
+                }
+
+                using (var tempMainIndexWriter = indexWriter)
                 using (var tempMainIndex = new LuceneIndex(_loggerFactory, luceneIndex.Name, new TempOptions(), tempMainIndexWriter))
                 using (var tempLocalDirectory = FSDirectory.Open(localLuceneIndexFolder, LockFactory.GetLockFactory(localLuceneIndexFolder)))
                 using (var replicator = new ExamineReplicator(_loggerFactory, tempMainIndex, tempLocalDirectory, tempDir))
@@ -98,6 +131,21 @@ namespace Examine.Lucene.Directories
             {
                 _replicator?.Dispose();
             }
+        }
+
+        private IndexWriter GetIndexWriter(Directory mainDir, OpenMode openMode)
+        {
+            var indexWriter = new IndexWriter(
+                mainDir,
+                new IndexWriterConfig(
+                    LuceneInfo.CurrentVersion,
+                    new StandardAnalyzer(LuceneInfo.CurrentVersion))
+                {
+                    OpenMode = openMode,
+                    IndexDeletionPolicy = new SnapshotDeletionPolicy(new KeepOnlyLastCommitDeletionPolicy())
+                });
+
+            return indexWriter;
         }
 
         private class TempOptions : IOptionsMonitor<LuceneDirectoryIndexOptions>
